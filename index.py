b2f6329b58fdb7f6476843d6c47e5885e997bcf2
Paanelada22 <murilohenriuq@gmail.com>
2025-03-27 16:49:33 -0400

    Add files via upload

---
+++ index.py
@@ -0,0 +1,71 @@
+#Isto é um comentário
+
+# idade=6
+# altura=0.50
+# peso=5.950
+# nome="Francisco"
+# print(nome)
+# nome=nome+" é uma boa pessoa"
+# print(nome)
+# nome=input("insira seu nome: ")
+# idade=int(input("Insira sua idade: "))#int=inteiro, str=sting, float= 
+# peso=float(input("Insira seu peso: "))
+
+# print(f"{nome}, {idade} anos, {peso}kg")
+
+# pedaco_pizza=5
+# cliente="John"
+
+# print(type(pedaco_pizza))  #o comando type mostra o tipo das variavéis
+# print(type(cliente))
+
+
+# pedaco_pizza=int(input("informe quantos pedaços comeu: "))      #O python le tudo como string a não ser que seja definida como outro tipo, ex: int, float
+# print(type(pedaco_pizza))
+
+# a=456
+# A="SAl"
+# print(a)
+# print(A)
+
+# fruta1,fruta3,fruta2="Laranja","Maça","Banana"
+
+# print(fruta1,fruta2,fruta3)
+
+# num1=int(input("Insira o primeiro número: "))
+# num2=int(input("Insira o primeiro número: "))
+# print(num1+num2)
+
+# nome=input("Qual o vingador mais forte? ")
+# print(f"Olá, {nome}")
+
+# if nome == "hulk":
+#     print ("Bem vindo, vingador mais forte")
+# else:
+#     print ("Você não é o vingador mais forte")
+#     print ("Acesso negado!!")
+# contador=1
+# while contador<=100 :
+
+#     print(f"Francisco é uma pessoa legal {contador}")
+#     contador=contador+1
+# x=int(input("Insira um número: "))
+# # if x>2 and x<10:
+# #     print("O número está entre 2 e 10")
+
+# if x<2 or x>10:
+#     print("O número ou é menor 2 ou maior que 10")
+
+# if not x>10:
+#     print (f"{x}, é menor que 10")
+#     if x == 10:
+#         print (f"{x}, é 10")
+# else:
+#     print (f"{x}, é maior que 10")
+# y=8
+# if x>2 and (y>10 or not x == 5):
+#     print("Condição complexa atendida")
+# else:
+#     print("Condição não atendida")
+ 
+#A ordem na qual o python avalia os operadores lógicos é: "Not", "And" e por fim o "or"
