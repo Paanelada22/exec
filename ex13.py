9152fd92c1f856ebdc432ba9a0b164bf9b7db230
Paanelada22 <murilohenriuq@gmail.com>
2025-04-03 15:41:36 -0300

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,7 @@
+nome=input("coloque o nome do aluno")
+idade= int(input("coloque sua idade"))
+turma= input("qual turma pertence")
+if idade>=6:
+            print(f"aluno cadastrado com sucesso [nome], [idade] anos, turma [turma]")
+else:
+            print("a criança ainda nao tem idade suficiente para estudar conosco")
---
+++ ex14.py
@@ -0,0 +1,12 @@
+
+
+
+nome=input("coloque o nome de usuario")
+senha=input("coloque a senha")
+
+if senha=="09876" and nome=="maike":
+    print("login realizado")
+else:
+    print("login nao realizado")
+    
+
